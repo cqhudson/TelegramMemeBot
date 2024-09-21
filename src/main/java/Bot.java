f98bec13e31208eafffb1dec666411622666066c
Connor Hudson <connorqhudson2020@gmail.com>
2024-09-21 00:04:36 -0500

    Create storeInDatabase method, which handles the SQLite logic (DOES NOT WORK YET)

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -118,4 +118,111 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+    private void storeInDatabase(Message message) {
+
+        final String databasePath = ("jdbc:sqlite:" + dotenv.get("TELEGRAM_BOT_DATABASE_PATH"));
+        Connection connection = null;
+
+        // STEP 1 //
+        try {
+            // Create a new database file if it does not exist, otherwise connect to a pre-existing database.
+            connection = DriverManager.getConnection(databasePath);
+
+        } catch (SQLException e) {
+            System.out.println("STEP 1: " + e.getMessage());
+            return;
+        }
+
+        // STEP 2 //
+        try {
+            // Create the "users" table if it does not exist.
+            Statement statement = connection.createStatement();
+
+            // The backslash gets rid of the newline character in multi-line text blocks.
+            // https://stackoverflow.com/questions/59310303/java-13-triple-quote-text-block-without-newlines
+            final String sqlStatement = """
+                    CREATE TABLE IF NOT EXISTS users ( \
+                    telegram_user_id BIGINT PRIMARY KEY NOT NULL, \
+                    telegram_user_username TEXT UNIQUE NOT NULL, \
+                    telegram_user_first_name TEXT, \
+                    telegram_user_last_name TEXT, \
+                    telegram_user_message_count INTEGER NOT NULL DEFAULT 1 \
+                    );""".trim();
+
+            statement.execute(sqlStatement);
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println("STEP 2: " + e.getMessage());
+            return;
+        }
+
+        // STEP 3 //
+        try {
+            // Add a record to the database.
+            Statement statement = connection.createStatement();
+
+            // Get the data to store in record
+            final String telegram_user_id = message.getFrom().getId().toString();
+            final String telegram_user_username = message.getFrom().getUserName();
+            final String telegram_user_first_name = message.getFrom().getFirstName();
+            final String telegram_user_last_name = message.getFrom().getLastName();
+
+            /*
+            // Query to check for existing record, assign it an alias 'exists'
+            final String sqlQueryRecordExists = String.format("""
+                    
+                    SELECT COUNT(*) > 0 AS exists FROM users WHERE telegram_user_id = %s; \
+                    """, telegram_user_id).trim();
+            ResultSet resultSet = statement.executeQuery(sqlQueryRecordExists);
+            */
+
+            final String sqlQueryRecordExists = "SELECT COUNT(*) > 0 FROM users WHERE telegram_user_id = ?";
+            ResultSet resultSet = null;
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQueryRecordExists)) {
+                preparedStatement.setString(1, telegram_user_id);
+                resultSet = preparedStatement.executeQuery();
+            } catch (SQLException e) {
+                System.out.println("STEP 3, inner try block: " + e.getMessage());
+                return;
+            }
+
+            // Check if a record already exists
+            if (resultSet.next()) {
+                int exists = resultSet.getInt("exists");
+
+                if (exists == 1) {
+                    // Record already exists, update the existing record
+                    final String sqlStatement = String.format("""
+                            UPDATE users \
+                            SET telegram_user_username = '%s', \
+                            telegram_user_first_name = '%s', \
+                            telegram_user_last_name = '%s', \
+                            telegram_user_message_count = telegram_user_message_count + 1 \
+                            WHERE telegram_user_id = %s; \
+                            """, telegram_user_username, telegram_user_first_name, telegram_user_last_name, telegram_user_id).trim();
+
+                    statement.execute(sqlStatement);
+
+                } else {
+                    // Record does not exist, create a new one
+                    final String sqlStatement = String.format("""
+                            INSERT INTO users (telegram_user_id, telegram_user_username, telegram_user_first_name, telegram_user_last_name) \
+                            VALUES (%s, '%s', '%s', '%s'); \
+                            """, telegram_user_id, telegram_user_username, telegram_user_first_name, telegram_user_last_name).trim();
+
+                    statement.execute(sqlStatement);
+                }
+
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println("STEP 3: " + e.getMessage());
+            return;
+        }
+
+    }
+
 }
