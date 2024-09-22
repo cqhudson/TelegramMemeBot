cee3c80a80cc79002c0f882aa13f0b6fb45eaf54
Connor Hudson <connorqhudson2020@gmail.com>
2024-09-22 12:29:59 -0500

    Add new method to get JSON data from any given subreddit

--- src/main/java/Meme.java
+++ src/main/java/Meme.java
@@ -15,20 +15,35 @@ public class Meme {
 
         System.out.println("Fetching meme JSON data from " + MEME_API_URI);
 
+        getJsonData(MEME_API_URI);
+    }
+
+    public void getRandomMemeJsonFromSubreddit(String subreddit) {
+
+        final String endpoint = MEME_API_URI + "/" + subreddit;
+
+        System.out.println("Fetching meme JSON from " + endpoint);
+
+        getJsonData(endpoint);
+    }
+
+    private void getJsonData(String endpoint) {
         try (HttpClient client = HttpClient.newHttpClient()) {
+            // Create the GET request
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(MEME_API_URI))
+                    .uri(URI.create(endpoint))
                     .GET()
                     .build();
             try {
+                // Send the GET request
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                // Parse the Json into a MemeJson object
                 memeJson = new Gson().fromJson(response.body(), MemeJson.class);
-                System.out.println("Successfully fetched meme JSON date.");
+                System.out.println("Successfully fetched random meme JSON data.");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-        }
-         catch (UncheckedIOException ex) {
+        } catch (UncheckedIOException ex) {
             throw new RuntimeException(ex.getCause());
         }
     }
